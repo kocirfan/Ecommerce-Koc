fa926658f5ef29d34097c8b0d784d86b91921211
kocirfan <irfnk83@gmail.com>
2022-08-23 23:37:05 +0300

    pageable & search

--- Library/src/main/java/com/kocirfan/library/service/ProductService.java
+++ Library/src/main/java/com/kocirfan/library/service/ProductService.java
@@ -2,6 +2,7 @@ package com.kocirfan.library.service;
 
 import com.kocirfan.library.dto.ProductDto;
 import com.kocirfan.library.model.Product;
+import org.springframework.data.domain.Page;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -10,7 +11,15 @@ public interface ProductService {
 
     List<ProductDto> findAll();
     Product save(MultipartFile imageProduct, ProductDto productDto);
-    Product update(ProductDto productDto);
+    Product update(MultipartFile imageProduct, ProductDto productDto);
     void deleteById(Long id);
     void enableById(Long id);
+    ProductDto getById(Long id);
+
+    //pageable
+    Page<Product> pageProducts(int pageNo);
+
+    //search
+    Page<Product> searchProducts(int pageNo, String keyword);
+
 }
--- Library/src/main/java/com/kocirfan/library/service/impl/ProductServiceImpl.java
+++ Library/src/main/java/com/kocirfan/library/service/impl/ProductServiceImpl.java
@@ -6,6 +6,9 @@ import com.kocirfan.library.repository.ProductRepository;
 import com.kocirfan.library.service.ProductService;
 import com.kocirfan.library.utils.ImageUpload;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -26,7 +29,7 @@ public class ProductServiceImpl implements ProductService {
     public List<ProductDto> findAll() {
         List<ProductDto> productDtoList = new ArrayList<>();
         List<Product> products = productRepository.findAll();
-        for(Product product: products){
+        for (Product product : products) {
             ProductDto productDto = new ProductDto();
             productDto.setId(product.getId());
             productDto.setName(product.getName());
@@ -47,10 +50,10 @@ public class ProductServiceImpl implements ProductService {
     public Product save(MultipartFile imageProduct, ProductDto productDto) {
         try {
             Product product = new Product();
-            if(imageProduct == null){
+            if (imageProduct == null) {
                 product.setImage(null);
-            }else {
-                if(imageUpload.uploadImage(imageProduct)){
+            } else {
+                if (imageUpload.uploadImage(imageProduct)) {
                     System.out.println("upload successfully");
                 }
 
@@ -65,7 +68,7 @@ public class ProductServiceImpl implements ProductService {
             product.set_deleted(false);
             return productRepository.save(product);
 
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
@@ -73,17 +76,83 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
-    public Product update(ProductDto productDto) {
-        return null;
+    public Product update(MultipartFile imageProduct, ProductDto productDto) {
+        try {
+            Product product = productRepository.getById(productDto.getId());
+            if (imageProduct == null) {
+                product.setImage(productDto.getImage());
+            } else {
+                if (imageUpload.checkExisted(imageProduct) == false) {
+                    imageUpload.uploadImage(imageProduct);
+                    // System.out.println("upload to folder");
+                }
+                //System.out.println("Image existed");
+                product.setImage(Base64.getEncoder().encodeToString(imageProduct.getBytes()));
+            }
+            product.setName(productDto.getName());
+            product.setDescription(productDto.getDescription());
+            product.setSalePrice(productDto.getSalePrice());
+            product.setCostPrice(productDto.getCostPrice());
+            product.setCurrentQuantity(productDto.getCurrentQuantity());
+            product.setCategory(productDto.getCategory());
+            // return product;
+            return productRepository.save(product);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+
     }
 
     @Override
     public void deleteById(Long id) {
 
+        Product product = productRepository.getById(id);
+        product.set_deleted(true);
+        product.set_activated(false);
+        productRepository.save(product);
     }
 
     @Override
     public void enableById(Long id) {
 
+        Product product = productRepository.getById(id);
+        product.set_activated(true);
+        product.set_deleted(false);
+        productRepository.save(product);
+    }
+
+    @Override
+    public ProductDto getById(Long id) {
+        Product product = productRepository.findById(id).get();
+        ProductDto productDto = new ProductDto();
+        productDto.setId(product.getId());
+        productDto.setName(product.getName());
+        productDto.setDescription(product.getDescription());
+        productDto.setCurrentQuantity(product.getCurrentQuantity());
+        productDto.setCategory(product.getCategory());
+        productDto.setSalePrice(product.getSalePrice());
+        productDto.setCostPrice(product.getCostPrice());
+        productDto.setImage(product.getImage());
+        productDto.setDeleted(product.is_deleted());
+        productDto.setActivated(product.is_activated());
+        return productDto;
+    }
+
+    //pageable
+    @Override
+    public Page<Product> pageProducts(int pageNo) {
+       Pageable pageable = PageRequest.of(pageNo, 5);
+       Page<Product> productsPage = productRepository.pageProduct(pageable);
+       return productsPage;
+    }
+
+    //search
+    @Override
+    public Page<Product> searchProducts(int pageNo, String keyword) {
+        Pageable pageable = PageRequest.of(pageNo, 5);
+        Page<Product> products = productRepository.searchProducts(keyword, pageable);
+        return products;
     }
 }
